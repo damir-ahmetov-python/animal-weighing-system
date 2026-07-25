e012d48b4cfb14f83c44b14b91564173385bcf1c
Damir <ds@gmail.com>
2026-07-25 23:23:03 +0500

    feat: add crud and schemas for animaltype, breed and animal (create/read/update)

--- backend/app/db/crud.py
+++ backend/app/db/crud.py
@@ -1,4 +1,6 @@
-from app.models import User
+import datetime
+
+from app.models import User, AnimalType, Breed, Animal
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -8,7 +10,7 @@ def create_user(
         email: str,
         hashed_password: str,
         activation_token: str | None = None
-) -> User | None:
+) -> User:
 
     user = User(
         login=login,
@@ -41,3 +43,94 @@ def get_by_id(session: Session, user_id: int) -> User | None:
     res = session.execute(select(User).where(User.id == user_id))
 
     return res.scalar_one_or_none()
+
+def get_by_activation_token(session: Session, token: str) -> User | None:
+    res = session.execute(select(User).where(User.activation_token == token))
+
+    return res.scalar_one_or_none()
+
+def create_animal_type(session: Session, name_type: str) -> AnimalType:
+    animal_type = AnimalType(name_type=name_type)
+
+    session.add(animal_type)
+
+    session.commit()
+    session.refresh(animal_type)
+
+    return animal_type
+
+def get_animal_type_by_id(session: Session, type_id: int) -> AnimalType | None:
+    res = session.execute(select(AnimalType).where(AnimalType.type_id == type_id))
+
+    return res.scalar_one_or_none()
+
+def get_animal_type_by_name(session: Session, name_type: str) -> AnimalType | None:
+    res = session.execute(select(AnimalType).where(AnimalType.name_type == name_type))
+
+    return res.scalar_one_or_none()
+
+def create_breed(session: Session, name: str, type_id: int) -> Breed:
+    breed = Breed(name=name, type_id=type_id)
+
+    session.add(breed)
+
+    session.commit()
+    session.refresh(breed)
+
+    return breed
+
+def get_breed_by_id(session: Session, breed_id: int) -> Breed | None:
+    res = session.execute(select(Breed).where(Breed.breed_id == breed_id))
+
+    return res.scalar_one_or_none()
+
+def create_animal(
+        session: Session,
+        inventory_number: str,
+        gender: str,
+        name: str,
+        arrival_date: datetime.date,
+        arrival_age_months: int,
+        breed_id: int,
+        parent_id: int | None = None
+) -> Animal:
+
+    animal = Animal(
+        inventory_number=inventory_number,
+        gender=gender,
+        name=name,
+        arrival_date=arrival_date,
+        arrival_age_months=arrival_age_months,
+        breed_id=breed_id,
+        parent_id=parent_id,
+    )
+
+    session.add(animal)
+
+    session.commit()
+    session.refresh(animal)
+
+    return animal
+
+def get_animal_by_id(session: Session, animal_id: int) -> Animal | None:
+    res = session.execute(select(Animal).where(Animal.animal_id == animal_id))
+
+    return res.scalar_one_or_none()
+
+def update_animal(session: Session, animal: Animal, data: dict) -> Animal:
+    for k, v in data.items():
+        setattr(animal, k, v)
+
+    session.commit()
+    session.refresh(animal)
+
+    return animal
+
+def delete_animal(session: Session, animal_id: int) -> bool:
+    animal = get_animal_by_id(session=session, animal_id=animal_id)
+
+    session.delete(animal)
+
+    session.commit()
+
+
--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from pydantic import BaseModel, EmailStr, ConfigDict, Field
 import datetime
 
@@ -15,8 +17,57 @@ class UserResponse(BaseModel):
     is_active: bool
     role: str
     created_at: datetime.datetime
-    weightings: list
 
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+class AnimalTypeCreate(BaseModel):
+    name_type: str
+
+class AnimalTypeResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    type_id: int
+    name_type: str
+
+class BreedCreate(BaseModel):
+    name: str
+    type_id: int
+
+class BreedResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    breed_id: int
+    name: str
+
+class AnimalCreate(BaseModel):
+    inventory_number: str
+    gender: Literal['male', 'female']
+    name: str | None = None
+    arrival_date: datetime.date
+    arrival_age_months: int | None = None
+    breed_id: int
+    parent_id: int | None = None
+
+class AnimalUpdate(BaseModel):
+    inventory_number: str | None = None
+    gender: Literal['male', 'female'] | None = None
+    name: str | None = None
+    arrival_date: datetime.date | None = None
+    arrival_age_months: int | None = None
+    breed_id: int | None = None
+    parent_id: int | None = None
+
+class AnimalResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    animal_id: int
+    inventory_number: str
+    gender: Literal['male', 'female']
+    name: str | None = None
+    arrival_date: datetime.date
+    arrival_age_months: int | None = None
+    breed_id: int
+    parent_id: int | None = None
+
